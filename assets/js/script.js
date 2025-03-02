cbc992076de3648c5e8aab97c32c5912e07dde60
Eduard0w <dudusouza5595@gmail.com>
2025-03-02 18:44:15 -0300

    add the project(GitHub)

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let log = new Log(document.querySelector(".log"));
+
+let char = new Knight("Juguerbal");
+
+// console.log('Mago: '+char.name);
+// console.log('Sua vida: '+char.life);
+// console.log('Sua força é: '+char.attack);
+
+let Monster = new LittleMonster();
+// console.log(Monster.name)
+
+const stage = new Stage(
+  char,
+  Monster,
+  document.querySelector("#char"),
+  document.querySelector("#monster"),
+  log
+);
+
+stage.start();
